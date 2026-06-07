82590b90348f0083cfc993dc3e42948bd14354f2
Fabian2611 <fabian.schwarz2010@gmail.com>
2026-06-07 12:40:17 +0200

    finished 3d designs

---
+++ target/debug/deps/macropad-b2d138d8c29224a9.d
@@ -0,0 +1,5 @@
+/home/fabian/files/Source/Rust/macropad/target/debug/deps/macropad-b2d138d8c29224a9.d: src/main.rs
+
+/home/fabian/files/Source/Rust/macropad/target/debug/deps/libmacropad-b2d138d8c29224a9.rmeta: src/main.rs
+
+src/main.rs:
