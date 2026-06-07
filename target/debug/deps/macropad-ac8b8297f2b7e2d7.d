20472f80c2f064dcee588468ad3a4ba3dfb6e4b1
Fabian2611 <fabian.schwarz2010@gmail.com>
2026-06-07 13:14:36 +0200

    Welp, fw is almost done, but I just read that only QMK, KMK and ZMK are allowed.

--- target/debug/deps/macropad-ac8b8297f2b7e2d7.d
+++
@@ -1,5 +0,0 @@
-/home/fabian/files/Source/Rust/macropad/target/debug/deps/macropad-ac8b8297f2b7e2d7.d: src/main.rs
-
-/home/fabian/files/Source/Rust/macropad/target/debug/deps/libmacropad-ac8b8297f2b7e2d7.rmeta: src/main.rs
-
-src/main.rs:
--- target/debug/deps/macropad-b2d138d8c29224a9.d
+++
@@ -1,5 +0,0 @@
-/home/fabian/files/Source/Rust/macropad/target/debug/deps/macropad-b2d138d8c29224a9.d: src/main.rs
-
-/home/fabian/files/Source/Rust/macropad/target/debug/deps/libmacropad-b2d138d8c29224a9.rmeta: src/main.rs
-
-src/main.rs:
